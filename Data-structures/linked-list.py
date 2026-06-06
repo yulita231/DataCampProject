f9716df7a6e7bf94c7945184e53c754901b84cbf
Yulita <yulitarahma120@gmail.com>
2026-06-06 21:06:34 +0700

    Implement node removal at the beginning of linked list
    Added a method to remove the first node from the linked list and updated comments for clarity.

--- Data-structures/linked-list.py
+++ Data-structures/linked-list.py
@@ -1,4 +1,4 @@
-'''Steps to insert a node at the beginning:
+'''No 1- Steps to insert a node at the beginning:
 1. Initialize a new node with the given data.
 2. Set the 'next' pointer of the new node to the current head of the list.
 3. Update the head of the list to point to the new node.
@@ -15,3 +15,13 @@ def insert_at_beginning(self, data):
     else:
       self.tail = new_node      
       self.head = new_node
+
+'''No 2 - Removing the first node from a linked list'''
+class LinkedList:
+  def __init__(self):
+    self.head = None
+    self.tail = None
+    
+  def remove_at_beginning(self):
+    # The "next" node of the head becomes the new head node
+    self.head = self.head.next
