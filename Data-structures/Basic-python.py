4991fad14e7f93777ac7600f812a98ef31e641b6
Yulita <yulitarahma120@gmail.com>
2026-06-13 13:03:05 +0700

    add multiple case in pyhton

---
+++ Data-structures/Basic-python.py
@@ -0,0 +1,10 @@
+# Create the variables monthly_savings and num_months
+monthly_savings = 10
+num_months = 4
+
+
+# Multiply monthly_savings and num_months
+new_savings = monthly_savings * num_months
+
+# Print new_savings
+print(new_savings)
