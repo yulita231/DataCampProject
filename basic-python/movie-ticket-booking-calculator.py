8eabba94d8e918be5678d99a3cbd9c9067ff5144
Yulita <yulitarahma120@gmail.com>
2026-06-17 16:56:47 +0700

    add conditional statement to check eligible ticket

---
+++ basic-python/movie-ticket-booking-calculator.py
@@ -0,0 +1,13 @@
+#practice how to use booleans and conditional statements in Python by building a movie ticket booking calculator.
+base_price = 15
+age = 21
+seat_type = 'Gold'
+show_time = 'Evening'
+
+if age > 17:
+    print('User is eligible to book a ticket')
+
+if age >= 21:
+    print('User is eligible for Evening shows')
+else:
+    print('User is not eligible for Evening shows')
