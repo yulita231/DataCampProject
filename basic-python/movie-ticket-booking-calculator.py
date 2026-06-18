1040712bc160d7825f596cc5328b3bc1dcc3b8e9
Yulita <yulitarahma120@gmail.com>
2026-06-18 14:41:08 +0700

    add new variable extra_charges

--- basic-python/movie-ticket-booking-calculator.py
+++ basic-python/movie-ticket-booking-calculator.py
@@ -11,3 +11,21 @@ if age >= 21:
     print('User is eligible for Evening shows')
 else:
     print('User is not eligible for Evening shows')
+    
+is_member = True
+is_weekend = False
+
+discount = 0
+
+if is_member and age >= 21:
+    discount=3
+    print('User qualifies for membership discount')
+else:
+    print('User does not qualify for membership discount')
+print('Discount:', discount)
+
+extra_charges=0
+
+if is_weekend:
+    extra_charges=2
+    print('Extra charges will be applied')
