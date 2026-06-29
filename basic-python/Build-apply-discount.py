ed741c3f4aedb06f415c3e2df9f10813840983ab
Yulita <yulitarahma120@gmail.com>
2026-06-29 09:25:30 +0700

    add try except to apply the function

--- basic-python/Build-apply-discount.py
+++ basic-python/Build-apply-discount.py
@@ -1,4 +1,5 @@
 
+
 #define a function named apply_discount.
 def apply_discount(price, discount):
     #check if the price and discount are valid numbers
@@ -13,3 +14,17 @@ def apply_discount(price, discount):
     #check if the discount is between 0 and 100
     elif discount < 0 or discount > 100:
         return ('The discount should be between 0 and 100')
+    #if all the conditions are met, calculate the discounted price
+    else:
+        potongan = price * (discount / 100)
+        harga_akhir = price - potongan
+        return harga_akhir
+    
+try:
+        price=float(input('Enter the price: '))
+        discount=float(input('Enter the discount: '))
+        last_price=apply_discount(price, discount)
+        print(f'The final price after applying the discount is: {last_price}')
+        
+except ValueError:
+        print('Please enter valid numbers for price and discount.')
